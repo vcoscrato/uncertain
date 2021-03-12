b121dae549208b3fb265e900c07cc9a353593dd1
Victor Coscrato <vcoscrato@gmail.com>
2021-03-12 02:22:03 -0300

    fix ignores

--- Legacy/Reliable/__init__.py
+++
@@ -1,8 +0,0 @@
-from .metrics import rmse, mse, mae, fcp, kendallW, RPI, build_intervals, build_quantiles, precision, recall, RRI
-from .algobase import ReliableAlgoBase, ReliablePrediction, ReliableRecommendation
-from .models import EMF, RMF, Heuristic_reliability, Model_reliability
-from .RankingAggregation import RankingAggregation
-
-__version__ = "0.2-dev"
-
-__all__ = ["models", "metrics"]
