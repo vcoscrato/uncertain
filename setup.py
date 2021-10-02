74f5607aefc494c898b3d4117d6d8dff29a18796
Victor Coscrato <vcoscrato@gmail.com>
2021-10-02 19:04:27 -0300

    fix

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='uncertain',
-    version='0.9.2',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=['torch>=1.7.0', 'numpy', 'scipy', 'pytorch-lightning', 'h5py', 'pandas', 'requests', 'matplotlib',
                       'scikit-learn'],
     license='MIT',
-    classifiers=['Development Status :: 3 - Alpha',
+    classifiers=['Development Status :: 1 - Beta',
                  'License :: OSI Approved :: MIT License',
                  'Topic :: Scientific/Engineering :: Artificial Intelligence'],
 )
